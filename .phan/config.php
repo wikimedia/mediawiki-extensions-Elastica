3cd3f183becdc58b672152238a30b2a7ff108315
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2024-02-09 14:29:59 +0000

    build: Updating dependencies
    composer:
    * mediawiki/mediawiki-phan-config: 0.12.1 → 0.14.0
    
    npm:
    * grunt-banana-checker: 0.11.0 → 0.11.1
    
    Change-Id: I3275841a669c4b8b9c2594e302288984e2b96fd3

--- .phan/config.php
+++ .phan/config.php
@@ -2,4 +2,7 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
+// To migrate later
+$cfg['suppress_issue_types'][] = 'MediaWikiNoBaseException';
+
 return $cfg;
